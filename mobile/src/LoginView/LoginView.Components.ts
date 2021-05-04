600a10d3cb0ec182b474e88797a0080064a2b32b
MacPiston <mcpiston@gmail.com>
2021-05-04 15:50:49 +0200

    15:50 - layout changes, added Modal

--- mobile/src/LoginView/LoginView.Components.ts
+++ mobile/src/LoginView/LoginView.Components.ts
@@ -1,7 +1,56 @@
 /* eslint-disable import/prefer-default-export */
-import { Text } from 'react-native';
+import { Text, View, TextInput, Image } from 'react-native';
 import styled from 'styled-components';
 
+export const LoginViewContainer = styled(View)`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const HeaderText = styled(Text)`
+  flex: 1.5;
+  margin-top: 20px;
   font-weight: 600;
+  font-size: 50px;
+  /* border: 1px solid black; */
+`;
+
+export const InputContainer = styled(View)`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  flex: 2.2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid black; */
+  height: 55%;
+  width: 100%;
+`;
+
+export const StyledTextInput = styled(TextInput)`
+  background-color: white;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 6px;
+  width: 90%;
+  height: 40%;
+  font-size: 22px;
+`;
+
+export const StyledImage = styled(Image)`
+  flex: 6;
+  aspect-ratio: 1;
+`;
+
+export const ButtonsContainer = styled(View)`
+  flex: 1;
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+  align-content: space-between;
+  /* border: 1px solid black; */
 `;
--- mobile/src/LoginView/LoginView.tsx
+++ mobile/src/LoginView/LoginView.tsx
@@ -1,14 +1,114 @@
-import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, View, Pressable, Modal } from 'react-native';
 import { DivStyles } from './Stylesheets/Stylesheets';
-import { HeaderText } from './LoginView.Components';
+import {
+  HeaderText,
+  InputContainer,
+  LoginViewContainer,
+  StyledTextInput,
+  StyledImage,
+  ButtonsContainer,
+} from './LoginView.Components';
 
-const LoginView: React.FC = (props) => {
+interface ButtonProps {
+  primary?: boolean;
+  text: string;
+  onPress: () => void;
+}
+const CustomizedButton = ({ primary = false, text, onPress }: ButtonProps) => {
+  return (
+    <Pressable
+      style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}
+      onPress={onPress}
+    >
+      <Text style={primary ? { fontSize: 34 } : { fontSize: 20 }}>{text}</Text>
+    </Pressable>
+  );
+};
+
+interface ModalProps {
+  visible: boolean;
+  handleClose: () => void;
+}
+const InfoModal = ({ visible, handleClose }: ModalProps) => (
+  <Modal
+    animationType="fade"
+    visible={visible}
+    onRequestClose={handleClose}
+    transparent
+  >
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        marginHorizontal: 30,
+        marginVertical: 320,
+        borderRadius: 5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      }}
+      onTouchStart={handleClose}
+    >
+      <Text>Nie dostaniesz konta i chuj</Text>
+    </View>
+  </Modal>
+);
+
+const LoginView: React.FC = () => {
   const { Background } = DivStyles;
+  const [email, setEmail] = useState<string>('');
+  const [pwd, setPwd] = useState<string>('');
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  const handleLogin = () => {
+    console.log('Login');
+  };
+
   return (
     <View style={Background}>
       <SafeAreaView>
-        <HeaderText>LoginView</HeaderText>
+        <InfoModal
+          visible={modalVisible}
+          handleClose={() => setModalVisible(false)}
+        />
+        <LoginViewContainer>
+          <HeaderText>Door_system</HeaderText>
+          <StyledImage resizeMode="stretch" source={require('./doors.png')} />
+          <InputContainer>
+            <StyledTextInput
+              autoCompleteType="email"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              placeholder="Email address..."
+              onChange={setEmail}
+            />
+            <StyledTextInput
+              autoCompleteType="password"
+              autoCapitalize="none"
+              keyboardType="default"
+              secureTextEntry
+              placeholder="Password..."
+              onChange={setPwd}
+            />
+          </InputContainer>
+          <ButtonsContainer>
+            <CustomizedButton
+              text="Need account?"
+              onPress={() => setModalVisible(true)}
+            />
+            <CustomizedButton primary text="Login" onPress={handleLogin} />
+          </ButtonsContainer>
+        </LoginViewContainer>
       </SafeAreaView>
     </View>
   );
