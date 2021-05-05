e4fbb67ea2faf26ce3089925f367141723406305
Mateusz Migot <migotmateusz@gmail.com>
2021-05-05 17:44:44 +0200

    added connection with mysql database

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,25 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: "door_access"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    connection.query("SELECT * from users", function (error, results, fields) {
+        if (err) throw err;
+        console.log(results);
+        connection.end();
+    });
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
