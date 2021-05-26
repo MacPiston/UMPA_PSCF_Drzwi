e6d666cc8ff5d7eca51ec5d5d828b47680671fe5
MacPiston <mcpiston@gmail.com>
2021-05-26 20:35:43 +0200

    Revert "Delete App.tsx"
    This reverts commit 14d298e58e714441ac7830a1f18bfe2c052bbf72.

---
+++ mobile/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import LoginView from './src/LoginView/LoginView';
+import DoorsView from './src/DoorsView/DoorsView';
+
+const App = () => {
+  //return <LoginView />;
+  return <DoorsView />;
+};
+
+export default App;
